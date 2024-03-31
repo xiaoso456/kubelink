9604bc7fb3210235529d6b481119bd12a2c80536
xiaoso <xiaoso456@qq.com>
2024-03-31 18:25:55 +0800

    update uri and and syncTypeList interface

--- src/main/java/io/github/xiaoso456/kubelink/controller/SyncConfigController.java
+++ src/main/java/io/github/xiaoso456/kubelink/controller/SyncConfigController.java
@@ -2,6 +2,7 @@ package io.github.xiaoso456.kubelink.controller;
 
 import io.github.xiaoso456.kubelink.domain.ClusterConfig;
 import io.github.xiaoso456.kubelink.domain.SyncConfig;
+import io.github.xiaoso456.kubelink.enums.SyncType;
 import io.github.xiaoso456.kubelink.exception.LinkException;
 import io.github.xiaoso456.kubelink.service.ClusterConfigService;
 import io.github.xiaoso456.kubelink.service.SyncConfigService;
@@ -9,45 +10,51 @@ import io.kubernetes.client.openapi.models.VersionInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/sync/config")
+@RequestMapping("/sync")
 public class SyncConfigController {
 
     @Autowired
     private SyncConfigService syncConfigService;
 
-    @GetMapping("/{id}")
+    @GetMapping("/config/{id}")
     public SyncConfig get(@PathVariable Long id) {
         return syncConfigService.getById(id);
     }
 
-    @GetMapping("/list")
+    @GetMapping("/config/list")
     public List<SyncConfig> list() {
         return syncConfigService.list();
     }
 
-    @PostMapping
+    @PostMapping("/config")
     public boolean add(@RequestBody SyncConfig syncConfig) {
         return syncConfigService.save(syncConfig);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/config/{id}")
     public boolean update(@PathVariable Long id,@RequestBody SyncConfig syncConfig) {
         syncConfig.setId(id);
         return syncConfigService.updateById(syncConfig);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/config/{id}")
     public boolean delete(@PathVariable Long id) {
         return syncConfigService.removeById(id);
     }
 
-    @PostMapping("/{id}/sync-only")
+    @PostMapping("/config/{id}/sync-only")
     public void syncOnly(@PathVariable Long id) {
         SyncConfig syncConfig = syncConfigService.getById(id);
         syncConfigService.syncOnly(syncConfig);
+    }
+
+    @GetMapping("/sync-type/list")
+    public List<SyncType> listSyncType(){
+        return Arrays.asList(SyncType.values());
     }
 
 
