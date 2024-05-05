03a2d6be424cc63997737ec465d3b075031b7d6b
xiaoso <xiaoso456@qq.com>
2024-05-05 14:43:21 +0800

    ci-test

--- start.cmd
+++ start.cmd
@@ -1,4 +1,5 @@
 @echo off
 set JAVA_PATH="java/bin/java.exe"  
 set JAR_FILE="kube-link.jar"
-%JAVA_PATH% -jar %JAR_FILE%
+set JAR_CONFIG="configs/config/application.yml"
+%JAVA_PATH% -jar %JAR_FILE% --spring.config.location=file:%JAR_CONFIG%
