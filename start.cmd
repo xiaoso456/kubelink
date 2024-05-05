39a3b2b91a31dde707c30e55538e6293b0264cc7
xiaoso <xiaoso456@qq.com>
2024-05-05 15:15:11 +0800

    update init data and scripts

--- start.cmd
+++ start.cmd
@@ -3,3 +3,4 @@ set JAVA_PATH="java/bin/java.exe"
 set JAR_FILE="kube-link.jar"
 set JAR_CONFIG="configs/config/application.yml"
 %JAVA_PATH% -jar %JAR_FILE% --spring.config.location=file:%JAR_CONFIG%
+pause
